e1e900c2119329ed7a187a6332cbee5dbec95139
jeayoung <k01066624566@gmail.com>
2022-11-18 14:25:50 +0900

    add HttpMessageNotReadableException Handler

--- src/main/kotlin/com/practice/hexagonal/infraStructure/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/practice/hexagonal/infraStructure/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.practice.hexagonal.infraStructure.exception
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import javax.servlet.http.HttpServletRequest
@@ -18,5 +19,14 @@ class GlobalExceptionHandler {
         ex.printStackTrace()
         val errorResponse = ErrorResponse(ex.errorCode.code, ex.errorCode.msg)
         return ResponseEntity(errorResponse, HttpStatus.valueOf(ex.errorCode.code))
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun HttpMessageNotReadExceptionHandler(request: HttpServletRequest, response: HttpServletResponse, ex:HttpMessageNotReadableException): ResponseEntity<ErrorResponse>{
+        log.error(request.requestURI)
+        log.error(ex.message)
+        ex.printStackTrace()
+        val errorResponse = ErrorResponse(ErrorCode.BAD_REQUEST.code, ErrorCode.BAD_REQUEST.msg)
+        return ResponseEntity(errorResponse, HttpStatus.valueOf(ErrorCode.BAD_REQUEST.code))
     }
 }
